e76ecd2c79b560ffd69dd5bc2f25fddda9d75f1f
OrcClaSH <orcaclash@gmail.com>
2023-12-15 19:53:15 +0900

    feat: add DropLineImg component

--- src/shared/ui/img/index.ts
+++ src/shared/ui/img/index.ts
@@ -1,3 +1,4 @@
 export { EyeImg } from './eye-img';
 export { BracketsImg } from './brackets-img';
 export { DropZoneImg } from './drop-zone-img.tsx';
+export { DropLineImg } from './drop-line-img.tsx';
